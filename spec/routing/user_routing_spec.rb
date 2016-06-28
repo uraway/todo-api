8f8012ddfbc320eaede174054000abd085b9b442
uraway <masato.u0107@gmail.com>
2016-06-29 08:13:03 +0900

    post /v1/users to create a new user

--- spec/routing/user_routing_spec.rb
+++ spec/routing/user_routing_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe 'routing to user', type: :routing do
-  it 'routes /v1/signup to /v1/user#create' do
-    expect(post: '/v1/signup').to route_to(
+RSpec.describe 'routing to users', type: :routing do
+  it 'routes /v1/users to /v1/users#create' do
+    expect(post: '/v1/users').to route_to(
       format: :json, controller: 'v1/users', action: 'create'
     )
   end
